62dde9a7571ca8b79b9bc40a60015dad352cc5b8
Glenn Hartmann <hartmann.glenn@gmail.com>
2024-12-09 13:52:37 -0500

    add generalized pad() function from https://github.com/glennhartmann/aoc24/blob/22670e51f18d8f4de124eed3e99c1f27ccfaab9d/src/days/day_04.rs

--- src/lib.rs
+++ src/lib.rs
@@ -22,3 +22,26 @@ pub fn fwd_rev_incl_range(start: usize, end: usize) -> impl Iterator<Item = usiz
 
     iter::from_fn(move || if start > end { rev.next() } else { fwd.next() })
 }
+
+pub fn pad<T: Clone + Copy>(contents: &Vec<&[T]>, padding: usize, default: T) -> Vec<Vec<T>> {
+    let mut r = Vec::with_capacity(contents.len());
+    let mut prefix = vec![vec![default; contents[0].len() + padding * 2]; padding];
+    r.append(&mut prefix);
+
+    for line in contents {
+        let mut v = Vec::with_capacity(line.len() + padding * 2);
+        let mut prefix = vec![default; padding];
+        let mut middle = line.to_vec();
+        let mut suffix = vec![default; padding];
+        v.append(&mut prefix);
+        v.append(&mut middle);
+        v.append(&mut suffix);
+
+        r.push(v);
+    }
+
+    let mut suffix = vec![vec![default; contents[0].len() + padding * 2]; padding];
+    r.append(&mut suffix);
+
+    r
+}
