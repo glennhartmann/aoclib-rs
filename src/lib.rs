6b63bd2aba902909e4cc0e75d4f694a3cbbf1b01
Glenn Hartmann <hartmann.glenn@gmail.com>
2024-12-21 09:55:34 -0500

    Direction iterators.

--- src/lib.rs
+++ src/lib.rs
@@ -49,6 +49,45 @@ impl Direction {
         let (d_x, d_y) = self.delta();
         (usize_plus_i32(usizes.0, d_x), usize_plus_i32(usizes.1, d_y))
     }
+
+    pub fn iter() -> impl Iterator<Item = Direction> {
+        let mut d = Direction::Up;
+        let mut first = true;
+        iter::from_fn(move || {
+            if d == Direction::Up && !first {
+                return None;
+            }
+            first = false;
+
+            let r = d;
+            d = d.rotate_right();
+
+            Some(r)
+        })
+    }
+
+    pub fn iter_valid_usizes_deltas(
+        curr: (usize, usize),
+        size: (usize, usize),
+    ) -> impl Iterator<Item = (usize, usize)> {
+        let mut dir_iter = Direction::iter();
+        iter::from_fn(move || loop {
+            match dir_iter.next() {
+                None => return None,
+                Some(d) => {
+                    let (dx, dy) = d.delta();
+                    let next = (curr.0 as i32 + dx, curr.1 as i32 + dy);
+                    if next.0 >= 0
+                        && next.0 < size.0 as i32
+                        && next.1 >= 0
+                        && next.1 < size.1 as i32
+                    {
+                        return Some((next.0 as usize, next.1 as usize));
+                    }
+                }
+            }
+        })
+    }
 }
 
 #[macro_export]
