e2dcd1e065aabd8584e5cac904f9f774a42ddfcb
Glenn Hartmann <hartmann.glenn@gmail.com>
2024-12-26 12:53:53 -0500

    v0.0.11

--- src-files.js
+++ src-files.js
@@ -1,3 +1,3 @@
-var srcIndex = new Map(JSON.parse('[["anyhow",["",[],["backtrace.rs","chain.rs","context.rs","ensure.rs","error.rs","fmt.rs","kind.rs","lib.rs","macros.rs","ptr.rs","wrapper.rs"]]],["aoclib_rs",["",[],["dir.rs","lib.rs","trie.rs"]]]]'));
+var srcIndex = new Map(JSON.parse('[["anyhow",["",[],["backtrace.rs","chain.rs","context.rs","ensure.rs","error.rs","fmt.rs","kind.rs","lib.rs","macros.rs","ptr.rs","wrapper.rs"]]],["aoclib_rs",["",[],["dijkstra.rs","dir.rs","lib.rs","trie.rs"]]]]'));
 createSrcSidebar();
-//{"start":36,"fragment_lengths":[144,52]}
+//{"start":36,"fragment_lengths":[144,66]}
