0d5fa2c25796fb9bc6b473473476c29e64f9f276
Glenn Hartmann <hartmann.glenn@gmail.com>
2024-12-28 10:36:58 -0500

    Add Dir8 implementation for 8-direction (diagonals)

--- src/dir.rs
+++ src/dir.rs
@@ -2,72 +2,25 @@ use std::iter;
 
 use crate::usize_plus_i32;
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
-pub enum Direction {
-    Up,
-    Down,
-    Left,
-    Right,
-}
+pub trait Direction: Sized {
+    fn delta(self) -> (i32, i32);
+    fn rotate_right(self) -> Self;
+    fn rotate_left(self) -> Self;
+    fn rotate_right_90(self) -> Self;
+    fn rotate_left_90(self) -> Self;
+    fn opposite(self) -> Self;
+    fn iter() -> impl Iterator<Item = Self>;
 
-impl Direction {
-    pub fn delta(self) -> (i32, i32) {
-        match self {
-            Direction::Up => (0, -1),
-            Direction::Down => (0, 1),
-            Direction::Left => (-1, 0),
-            Direction::Right => (1, 0),
-        }
-    }
-
-    pub fn rotate_right(self) -> Direction {
-        match self {
-            Direction::Up => Direction::Right,
-            Direction::Down => Direction::Left,
-            Direction::Left => Direction::Up,
-            Direction::Right => Direction::Down,
-        }
-    }
-
-    pub fn rotate_left(self) -> Direction {
-        self.rotate_right().opposite()
-    }
-
-    pub fn opposite(self) -> Direction {
-        match self {
-            Direction::Up => Direction::Down,
-            Direction::Down => Direction::Up,
-            Direction::Left => Direction::Right,
-            Direction::Right => Direction::Left,
-        }
-    }
-
-    pub fn apply_delta_to_usizes(self, usizes: (usize, usize)) -> (usize, usize) {
+    fn apply_delta_to_usizes(self, usizes: (usize, usize)) -> (usize, usize) {
         let (d_x, d_y) = self.delta();
         (usize_plus_i32(usizes.0, d_x), usize_plus_i32(usizes.1, d_y))
     }
 
-    pub fn iter() -> impl Iterator<Item = Direction> {
-        let mut d = Direction::Up;
-        let mut first = true;
-        iter::from_fn(move || {
-            if d == Direction::Up && !first {
-                return None;
-            }
-            first = false;
-
-            let r = d;
-            d = d.rotate_right();
-
-            Some(r)
-        })
-    }
-
-    pub fn iter_valid_usizes_deltas(
+    fn iter_valid_usizes_deltas(
         curr: (usize, usize),
         size: (usize, usize),
     ) -> impl Iterator<Item = (usize, usize)> {
-        let mut dir_iter = Direction::iter();
+        let mut dir_iter = Self::iter();
         iter::from_fn(move || loop {
             match dir_iter.next() {
                 None => return None,
@@ -83,6 +36,164 @@ impl Direction {
                     }
                 }
             }
+        })
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+pub enum Dir4 {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction for Dir4 {
+    fn delta(self) -> (i32, i32) {
+        match self {
+            Dir4::Up => (0, -1),
+            Dir4::Down => (0, 1),
+            Dir4::Left => (-1, 0),
+            Dir4::Right => (1, 0),
+        }
+    }
+
+    fn rotate_right(self) -> Dir4 {
+        match self {
+            Dir4::Up => Dir4::Right,
+            Dir4::Down => Dir4::Left,
+            Dir4::Left => Dir4::Up,
+            Dir4::Right => Dir4::Down,
+        }
+    }
+
+    fn rotate_left(self) -> Dir4 {
+        self.rotate_right().opposite()
+    }
+
+    fn rotate_right_90(self) -> Dir4 {
+        self.rotate_right()
+    }
+
+    fn rotate_left_90(self) -> Dir4 {
+        self.rotate_left()
+    }
+
+    fn opposite(self) -> Dir4 {
+        match self {
+            Dir4::Up => Dir4::Down,
+            Dir4::Down => Dir4::Up,
+            Dir4::Left => Dir4::Right,
+            Dir4::Right => Dir4::Left,
+        }
+    }
+
+    fn iter() -> impl Iterator<Item = Dir4> {
+        let mut d = Dir4::Up;
+        let mut first = true;
+        iter::from_fn(move || {
+            if d == Dir4::Up && !first {
+                return None;
+            }
+            first = false;
+
+            let r = d;
+            d = d.rotate_right();
+
+            Some(r)
+        })
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+pub enum Dir8 {
+    Dir4(Dir4),
+    UpRight,
+    UpLeft,
+    DownRight,
+    DownLeft,
+}
+
+impl Direction for Dir8 {
+    fn delta(self) -> (i32, i32) {
+        match self {
+            Dir8::Dir4(d4) => d4.delta(),
+            Dir8::UpRight => (1, -1),
+            Dir8::UpLeft => (-1, -1),
+            Dir8::DownRight => (1, 1),
+            Dir8::DownLeft => (-1, 1),
+        }
+    }
+
+    fn rotate_right(self) -> Dir8 {
+        match self {
+            Dir8::Dir4(Dir4::Up) => Dir8::UpRight,
+            Dir8::Dir4(Dir4::Down) => Dir8::DownLeft,
+            Dir8::Dir4(Dir4::Left) => Dir8::UpLeft,
+            Dir8::Dir4(Dir4::Right) => Dir8::DownRight,
+            Dir8::UpRight => Dir8::Dir4(Dir4::Right),
+            Dir8::UpLeft => Dir8::Dir4(Dir4::Up),
+            Dir8::DownRight => Dir8::Dir4(Dir4::Down),
+            Dir8::DownLeft => Dir8::Dir4(Dir4::Left),
+        }
+    }
+
+    fn rotate_left(self) -> Dir8 {
+        match self {
+            Dir8::Dir4(Dir4::Up) => Dir8::UpLeft,
+            Dir8::Dir4(Dir4::Down) => Dir8::DownRight,
+            Dir8::Dir4(Dir4::Left) => Dir8::DownLeft,
+            Dir8::Dir4(Dir4::Right) => Dir8::UpRight,
+            Dir8::UpRight => Dir8::Dir4(Dir4::Up),
+            Dir8::UpLeft => Dir8::Dir4(Dir4::Left),
+            Dir8::DownRight => Dir8::Dir4(Dir4::Right),
+            Dir8::DownLeft => Dir8::Dir4(Dir4::Down),
+        }
+    }
+
+    fn rotate_right_90(self) -> Dir8 {
+        match self {
+            Dir8::Dir4(d4) => Dir8::Dir4(d4.rotate_right()),
+            Dir8::UpRight => Dir8::DownRight,
+            Dir8::UpLeft => Dir8::UpRight,
+            Dir8::DownRight => Dir8::DownLeft,
+            Dir8::DownLeft => Dir8::UpLeft,
+        }
+    }
+
+    fn rotate_left_90(self) -> Dir8 {
+        match self {
+            Dir8::Dir4(d4) => Dir8::Dir4(d4.rotate_left()),
+            Dir8::UpRight => Dir8::UpLeft,
+            Dir8::UpLeft => Dir8::DownLeft,
+            Dir8::DownRight => Dir8::UpRight,
+            Dir8::DownLeft => Dir8::DownRight,
+        }
+    }
+
+    fn opposite(self) -> Dir8 {
+        match self {
+            Dir8::Dir4(d4) => Dir8::Dir4(d4.opposite()),
+            Dir8::UpRight => Dir8::DownLeft,
+            Dir8::UpLeft => Dir8::DownRight,
+            Dir8::DownRight => Dir8::UpLeft,
+            Dir8::DownLeft => Dir8::UpRight,
+        }
+    }
+
+    fn iter() -> impl Iterator<Item = Dir8> {
+        let mut d = Dir8::Dir4(Dir4::Up);
+        let mut first = true;
+        iter::from_fn(move || {
+            if d == Dir8::Dir4(Dir4::Up) && !first {
+                return None;
+            }
+            first = false;
+
+            let r = d;
+            d = d.rotate_right();
+
+            Some(r)
         })
     }
 }
