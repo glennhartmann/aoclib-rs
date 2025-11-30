6cb76aba787a82f4c5190b54f6fb500291fbf833
Glenn Hartmann <hartmann.glenn@gmail.com>
2025-11-30 13:15:06 -0500

    Lib

--- src/lib.rs
+++ src/lib.rs
@@ -53,6 +53,26 @@ pub fn pad<T: Clone + Copy>(contents: &Vec<&[T]>, padding: usize, default: T) ->
     r
 }
 
+// TODO: having 2 exactly identical functions is stupid. Find a way to fix this.
+pub fn pad_vec<T: Clone + Copy>(contents: &Vec<Vec<T>>, padding: usize, default: T) -> Vec<Vec<T>> {
+    let mut r = Vec::with_capacity(contents.len());
+    let mut prefix = vec![vec![default; contents[0].len() + padding * 2]; padding];
+    r.append(&mut prefix);
+
+    for line in contents {
+        let prefix = vec![default; padding];
+        let middle = line.to_vec();
+        let suffix = vec![default; padding];
+
+        r.push(vec![prefix, middle, suffix].into_iter().flatten().collect());
+    }
+
+    let mut suffix = vec![vec![default; contents[0].len() + padding * 2]; padding];
+    r.append(&mut suffix);
+
+    r
+}
+
 pub fn prep_io(contents: &mut String, day: u8) -> anyhow::Result<(BufWriter<File>, Vec<&str>)> {
     *contents = read_to_string(format!("inputs/{:02}.txt", day))?;
     let contents: Vec<&str> = contents.trim().lines().collect();
@@ -65,10 +85,7 @@ pub fn prep_io(contents: &mut String, day: u8) -> anyhow::Result<(BufWriter<File
 
 pub fn split_and_parse<T: FromStr>(s: &str, delim: &str) -> anyhow::Result<Vec<T>>
 where
-    <T as FromStr>::Err: std::error::Error,
-    <T as FromStr>::Err: Send,
-    <T as FromStr>::Err: Sync,
-    <T as FromStr>::Err: 'static,
+    <T as FromStr>::Err: std::error::Error + Send + Sync + 'static,
 {
     let mut v = Vec::new();
     for n in s.split(delim) {
@@ -112,6 +129,7 @@ pub fn u8_to_string(c: u8) -> String {
     String::from_utf8(vec![c]).unwrap()
 }
 
+// this function is pretty silly and new code should probably just use s.chars().
 pub fn split_by_char(s: &str) -> Vec<&str> {
     s.split("").filter(|c| !c.is_empty()).collect()
 }
@@ -127,4 +145,63 @@ where
     }
 
     None
+}
+
+// Euclidean Algorithm https://en.wikipedia.org/wiki/Greatest_common_divisor#Euclidean_algorithm
+pub fn gcd<T>(a: T, b: T) -> anyhow::Result<T>
+where
+    T: PartialEq + PartialOrd<i64> + fmt::Display + ops::Rem<Output = T> + Copy,
+{
+    if a < 0 || b < 0 {
+        return Err(anyhow::anyhow!(
+            "should only be used on positive numbers (got {} and {})",
+            a,
+            b
+        ));
+    }
+
+    if b == 0 {
+        return Ok(a);
+    }
+
+    gcd(b, a % b)
+}
+
+pub fn inc_selector(v: &mut [bool]) -> bool {
+    if v.iter().map(|&b| if b { 1 } else { 0 }).sum::<usize>() == v.len() {
+        return true;
+    }
+
+    let mut last = v.len() - 1;
+    loop {
+        v[last] = !v[last];
+
+        if v[last] {
+            break;
+        }
+
+        match last.checked_sub(1) {
+            None => break,
+            Some(l) => last = l,
+        }
+    }
+
+    false
+}
+
+// TODO
+pub fn fold_while<I, B, F>(it: I, init: B, mut f: F) -> B
+where
+    I: Iterator,
+    F: FnMut(B, I::Item) -> (B, bool),
+{
+    let mut b = init;
+    for v in it {
+        let (new_b, should_continue) = f(b, v);
+        b = new_b;
+        if !should_continue {
+            break;
+        }
+    }
+    b
 }
